8ce275f8646a9c79bcace7588dfa30f6f7f0703d
Nick <nvanalfen2@gmail.com>
2021-03-16 09:54:59 -0400

    Added imports for other files in directory

--- __init__.py
+++ __init__.py
@@ -2,3 +2,7 @@
 """
 
 from .rotate_vector_collection import rotate_vector_collection
+from . mcrotations import *
+from .vector_utilities import *
+from .rotations2d import *
+from .rotations3d import *
